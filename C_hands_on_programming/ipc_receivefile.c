9cc6704fea8650ceb8f3efa537a81929f7850e96
Kinh Truong <kinh.truong@unikie.com>
2022-02-18 05:42:35 -0800

    C files and Makefile setup

---
+++ C_hands_on_programming/ipc_receivefile.c
@@ -0,0 +1,16 @@
+/*
+ * ipc_receivefile.c
+ *
+ *  Created on: Feb 17, 2022
+ *      Author: kt
+ */
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+	while (1) {
+		printf("Hello\n");
+		sleep(2);
+	}
+	return 0;
+}
---
+++ C_hands_on_programming/ipc_sendfile.c
@@ -0,0 +1,13 @@
+/*
+ * ipc_sendfile.c
+ *
+ *  Created on: Feb 17, 2022
+ *      Author: kt
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello\n");
+	return 0;
+}
